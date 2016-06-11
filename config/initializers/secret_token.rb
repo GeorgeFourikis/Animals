8377dc063e2437c3f3c0cc162a9d5c72bb3c811e
GeorgeFourikis <you@example.cogit config --global user.email you@example.co>
2016-06-11 08:10:19 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Animals::Application.config.secret_key_base = 'cd958be73b5c1f58777eaa4ffe33bd8b6e5fea16895423c2d45583c73ec3da74ad3fce5513717d3b95be6c366d79f62a4f716641fe4215f6b12bb2581728b1ec'
