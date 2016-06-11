291e0d397fc552a52ab4e521c714da37a3b0e834
GeorgeFourikis <you@example.cogit config --global user.email you@example.co>
2016-06-11 08:35:27 +0000

    Added alter table with 2 columns

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160611081951) do
+ActiveRecord::Schema.define(version: 20160611083223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20160611081951) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "color"
+    t.integer  "age"
   end
 
 end
